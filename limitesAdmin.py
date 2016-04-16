f087fc3491fcf8da302a02dc10d1c767f22024e8
Alberto Roman <roman.j.alberto@gmail.com>
2016-04-16 21:13:40 +0200

    script shp a geojson

---
+++ limitesAdmin.py
@@ -0,0 +1,11 @@
+import os
+
+for root, dirs,files in os.walk('lineas_limite'):
+    for file in files:
+        if file.endswith(".shp") and file.startswith("recintos"):
+            file = file[:-4]
+            path = os.path.join(root, file)
+            cmd = 'ogr2ogr -f GeoJSON -t_srs crs:84 ' + path + '.geojson ' + path + '.shp'
+            os.system(cmd)
+            end = "Archivo: " + file + " convertido a GeoJSON!"
+            print(end)
