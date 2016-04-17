4c589303b2925cb86943ec40cd2fec17aa543839
Alberto Roman <roman.j.alberto@gmail.com>
2016-04-17 03:15:53 +0200

    update limitesAdmin.py

--- scripts/limitesAdmin.py
+++ scripts/limitesAdmin.py
@@ -5,19 +5,19 @@ import zipfile
 # Descargar el archivo zip, si hay otro archivo con el mismo nombre lo sobreescribe
 os.system("curl -# -f -O http://centrodedescargas.cnig.es/CentroDescargas/equipamiento/lineas_limite.zip")
 
-if not os.path.exists('lineas_limite'):
-    os.makedirs('lineas_limite')
-else:
-    shutil.rmtree('lineas_limite')
-    os.makedirs('lineas_limite')
-
+# Comprobamos que el archivo se ha desgargado
 if os.path.isfile('lineas_limite.zip'):
-    # Descomprimir el archivo
+    # Se crea el directorio lineas_limite si no existe y si exixte lo vaciamos.
+    if not os.path.exists('lineas_limite'):
+        os.makedirs('lineas_limite')
+    else:
+        shutil.rmtree('lineas_limite')
+        os.makedirs('lineas_limite')
+    # Se descomprime el archivo
     with zipfile.ZipFile('lineas_limite.zip', 'r') as z:
         z.extractall('lineas_limite')
     z.close()
-
-    # Buscar  archivos shp y convertir a GeoJSON
+    # Se buscan los archivos shapefile y se convierten a GeoJSON
     for root, dirs, files in os.walk('lineas_limite'):
         for file in files:
             if file.endswith(".shp") and file.startswith("recintos"):
